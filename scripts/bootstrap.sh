19bc00be4cc2a6e464483c36b2bfdb87640b476f
Sebastián J. Seba <ssebastianj@gmail.com>
2016-10-27 11:15:28 -0300

    Instalar curl al realizar bootstrap. Utilizar otra forma de instalar Composer.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -61,24 +61,17 @@ sudo a2enmod rewrite
 echo "Reiniciar Apache 2"
 service apache2 restart
 
+# install curl
+echo "Instalar curl"
+sudo apt-get install -y curl
+
 # install git
 echo "Instalar Git"
 sudo apt-get -y install git
 
 # install Composer
 echo "Instalar Composer"
-EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
-then
-    php composer-setup.php --quiet
-    RESULT=$?
-    rm composer-setup.php
-    exit $RESULT
-else
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
